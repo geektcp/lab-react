41a098d92b9966d74acb2cf6af7ae36e91ef9f04
geektcp <geektcp@163.com>
2022-11-18 11:27:26 +0800

    样式的操作

--- src/index.js
+++ src/index.js
@@ -1,11 +1,37 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDomClient from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const title =(
+    <h1>4444</h1>
+)
+
+const songs = [
+    {id:1, name:'dsdf上帝发誓地方'},
+    {id:2, name:'说的方法是'},
+    {id:3, name:'很反感和法规和'},
+
+]
+
+const list = (
+    <ul>
+        {songs.map(i => (
+            <li className="title">{i.name}</li>
+            )
+        )}
+    </ul>
+)
+
+const root = ReactDomClient.createRoot(document.getElementById('root'));
+
+// root.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// );
+
+ReactDOM.render(list, document.getElementById('root'));
+
+
