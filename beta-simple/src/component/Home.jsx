b6af09e95933ef07a28b1dc0969afb4f25f7cea3
geektcp <geektcp@163.com>
2023-01-02 17:16:43 +0800

    code review

--- beta-simple/src/component/Home.jsx
+++ beta-simple/src/component/Home.jsx
@@ -1,20 +1,24 @@
-import { HashRouter as Router, Route, Link } from "react-router-dom";
+import {HashRouter as Router, Route, Link} from "react-router-dom";
+
 var React = require('react');
+
 class Home extends React.Component {
     constructor(props) {
         super(props);
     }
 
     render() {
-        return <div>
-            <ul role="nav">
-                <li><Link to="/">tab1</Link></li>
-                <li><Link to="/tab2">tab2</Link></li>
-                <li><Link to="/tab3">tab3</Link></li>
-            </ul>
-            父组件
-            {this.props.children}
-        </div>;
+        return (
+            <div>
+                <ul role="nav">
+                    <li><Link to="/">tab1</Link></li>
+                    <li><Link to="/tab2">tab2</Link></li>
+                    <li><Link to="/tab3">tab3</Link></li>
+                </ul>
+                父组件
+                {this.props.children}
+            </div>
+        );
     }
 }
 
